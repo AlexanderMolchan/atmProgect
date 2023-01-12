48024051c7e618612ce4c99833778be12eeb9285
Alexander Molchan <79983103+AlexanderMolchan@users.noreply.github.com>
2023-01-12 21:52:29 +0300

    Update ViewController.swift

--- MapBank/Controllers/ViewController.swift
+++ MapBank/Controllers/ViewController.swift
@@ -36,6 +36,7 @@ class ViewController: UIViewController {
         locationManager.requestAlwaysAuthorization()
         locationManager.delegate = self
         locationManager.startUpdatingLocation()
+        print("Git Test")
     }
     
     private func getData() {
